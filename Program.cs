3906a8d253562bb51e23971c07440302739abf59
Mehmet Hasip <66031216+Mehmet-Hasip-Yabar@users.noreply.github.com>
2020-05-27 21:21:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        int sayi;
+        Console.WriteLine("Sayı girin:");
+        string girilen = Console.ReadLine();
+        sayi = Convert.ToInt32(girilen);
+        int kare = sayi * sayi;
+        Console.WriteLine("Sayının karesi:" + kare);
+        Console.ReadKey();
+    }
+}
